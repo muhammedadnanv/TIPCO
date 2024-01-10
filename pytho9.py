816cb9579b8ca8d40ab6960d2e2c675ab4d1cb28
Muhammed Adnan <147593005+muhammedadnanv@users.noreply.github.com>
2024-01-10 21:09:50 +0530

    Update pytho9.py

--- pytho9.py
+++ pytho9.py
@@ -1,23 +1,51 @@
 import requests
 
 def get_user_posts(username):
-    url = f"https://www.instagram.com/rifa_fathima__817/?__a=1"
+    url = f"https://www.instagram.com/{username}/?__a=1"
     response = requests.get(url)
     data = response.json()
-    posts = data["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"]
+    posts = data['graphql']['user']['edge_owner_to_timeline_media']['edges']
     return posts
 
-def report_user_posts(username):
-    posts = get_user_posts(username)
-    for post in posts:
-        post_id = post["node"]["id"]
-        post_url = f"https://www.instagram.com/p/{post_id}/"
-        likes = post["node"]["edge_liked_by"]["count"]
-        comments = post["node"]["edge_media_to_comment"]["count"]
-        print(f"Post URL: {post_url}")
-        print(f"Likes: {likes}")
-        print(f"Comments: {comments}")
-        print("")
+def get_user_followers(username):
+    url = f"https://www.instagram.com/{username}/?__a=1"
+    response = requests.get(url)
+    data = response.json()
+    followers = data['graphql']['user']['edge_followed_by']['count']
+    return followers
+
+def get_user_following(username):
+    url = f"https://www.instagram.com/{username}/?__a=1"
+    response = requests.get(url)
+    data = response.json()
+    following = data['graphql']['user']['edge_follow']['count']
+    return following
+
+def get_user_info(rifa_fathima__817):
+    url = f"https://www.instagram.com/{username}/?__a=1"
+    response = requests.get(url)
+    data = response.json()
+    user_info = {
+        'username': data['graphql']['user']['username'],
+        'full_name': data['graphql']['user']['full_name'],
+        'biography': data['graphql']['user']['biography'],
+        'followers': data['graphql']['user']['edge_followed_by']['count'],
+        'following': data['graphql']['user']['edge_follow']['count'],
+        'posts': data['graphql']['user']['edge_owner_to_timeline_media']['count']
+    }
+    return user_info
 
 # Example usage
-report_user_posts("instagram")
+username = 'example_user'
+user_posts = get_user_posts(username)
+user_followers = get_user_followers(username)
+user_following = get_user_following(username)
+user_info = get_user_info(username)
+
+print(f"Username: {user_info['username']}")
+print(f"Full Name: {user_info['full_name']}")
+print(f"Biography: {user_info['biography']}")
+print(f"Followers: {user_info['followers']}")
+print(f"Following: {user_info['following']}")
+print(f"Posts: {user_info['posts']}")
+
